e0ac25160c4aa95388f6c3a5e742621964ba22a1
Alejandro <alejandro_umartinez@outlook.com>
2024-11-22 18:57:16 -0600

    Added Skills and About Me sections to the portfolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,9 +6,26 @@ export default {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
+
       colors: {
-        primary: '#0b1120',
-        background: '#0f172a',
+        background: '',
+        enfasys: '#4ADFBF',
+        backgroundNav: '#0B0A11',
+        backgroundPurple: '#0e0c15 ',
+        backgroundBlue: '#17103e',
+      },
+      boxShadow: {
+        profile: 'inset 0 0 0 9px rgba(255, 255, 255, 0.1)',
+      },
+      keyframes: {
+        profileAnimate: {
+          '0%': { borderRadius: '60% 40% 30% 70% / 60% 30% 70% 40%' },
+          '50%': { borderRadius: '30% 60% 70% 40% / 50% 60% 30% 60%' },
+          '100%': { borderRadius: '60% 40% 30% 70% / 60% 30% 70% 40%' },
+        },
+      },
+      animation: {
+        profile: 'profileAnimate 8s ease-in-out infinite',
       },
     },
   },
